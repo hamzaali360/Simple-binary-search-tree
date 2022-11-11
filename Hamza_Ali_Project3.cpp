f51cf7530c6b3a45119deceafbcaa7e45dea5842
hamzaali360 <hamali1708@gmail.com>
2022-11-11 00:23:51 -0600

    Start project3

---
+++ Hamza_Ali_Project3.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+// #include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+    cout << "Welcome to the simple calculator!" << endl;
+
+    cout << "enter a command: " << endl;
+
+    string command;
+    cin >> command;
+    while(command != "q") {
+        if(command == "e") {
+
+        }
+        else if(command == "s"){
+
+        }
+        else if(command == "b"){
+            
+        }
+        else if(command == "c"){
+            
+        }
+        else if(command == "l"){
+            
+        }
+        else if(command == "a"){
+            
+        }
+        else if(command == "u"){
+            
+        }
+        else if(command == "i"){
+            
+        }
+        else {
+            cout << "This command does not exist" << endl;
+        }
+        cout << "Enter another command: ";
+        cin >> command;
+    }
+    return 0;
+}
